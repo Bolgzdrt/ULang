0574ab996c1253c5cf26bb40add8bb31392b68c3
Chris Jantzen <jantzeca@mail.uc.edu>
2021-01-19 15:10:37 -0500

    Initial setup

---
+++ application/ulang/server/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { authRouter } = require('./routes/authRoutes')
+
+const app = require('express')
+
+app.use(express.json())
+app.use('/auth', authRouter)
+
+const PORT = 8081
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
---
+++ application/ulang/server/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+// const authController = require('../controllers/authController')
+
+const router = Router()
+
+router
+  .route('/login')
+  .get(/* authController.login_get */)
+  .post(/* authController.login_post */)
+
+router
+  .route('/signup')
+  .get(/* authController.signup_get */)
+  .post(/* authController.signup_post */)
+
+router.get('/logout' /* authController.logout_get */)
+
+module.exports = {
+  authRouter: router
+}
