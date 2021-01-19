de062c375b829855ddb9bf3f8febcc41e01e8085
Chris Jantzen <jantzeca@mail.uc.edu>
2021-01-19 16:16:36 -0500

    Navbar, fix broken stuff, add color pallet in comment

--- application/ulang/src/App.vue
+++ application/ulang/src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* import Roboto font from Google */
+  font-family: /* Roboto, */ Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  /* I don't know if these work, but these are most of the main colors for the app */
+  /* --purple: #7666D8;
+  --white: #fff;
+  --accent-red: #ff6663;
+  --yellow: #faff00;
+  --gray: #ccc; */
 }
 </style>
---
+++ application/ulang/src/assets/svgs/plus.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="plusContainer" @click="onAddWordClick">
+    <svg
+      id="plus"
+      width="30"
+      height="30"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M27.0703 18.75H21.25V12.9297C21.25 12.2422 20.6875 11.6797 20 11.6797C19.3125 11.6797 18.75 12.2422 18.75 12.9297V18.75H12.9297C12.2422 18.75 11.6797 19.2188 11.6797 20C11.6797 20.7812 12.2656 21.25 12.9297 21.25H18.75V27.0703C18.75 27.8125 19.2969 28.3203 20 28.3203C20.7031 28.3203 21.25 27.7578 21.25 27.0703V21.25H27.0703C27.7578 21.25 28.3203 20.6875 28.3203 20C28.3203 19.3125 27.7578 18.75 27.0703 18.75Z"
+        fill="white"
+      />
+      <path
+        d="M20 5.9375C23.7578 5.9375 27.2891 7.39844 29.9453 10.0547C32.6016 12.7109 34.0625 16.2422 34.0625 20C34.0625 23.7578 32.6016 27.2891 29.9453 29.9453C27.2891 32.6016 23.7578 34.0625 20 34.0625C16.2422 34.0625 12.7109 32.6016 10.0547 29.9453C7.39844 27.2891 5.9375 23.7578 5.9375 20C5.9375 16.2422 7.39844 12.7109 10.0547 10.0547C12.7109 7.39844 16.2422 5.9375 20 5.9375ZM20 3.75C11.0234 3.75 3.75 11.0234 3.75 20C3.75 28.9766 11.0234 36.25 20 36.25C28.9766 36.25 36.25 28.9766 36.25 20C36.25 11.0234 28.9766 3.75 20 3.75Z"
+        fill="white"
+      />
+    </svg>
+    <div class="nav-modal" v-if="clicked">
+      <div><p>New Word</p></div>
+      <div><p>New Set</p></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'plus',
+  data() {
+    return {
+      clicked: false
+    }
+  },
+  methods: {
+    onAddWordClick(e) {
+      this.clicked = !this.clicked
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+#plusContainer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  cursor: pointer;
+}
+
+.nav-modal {
+  position: absolute;
+  bottom: -75px;
+  right: 0;
+  width: 150px; /* Possibly make this 200 when the SVGs are in */
+  height: 75px;
+  border: 1px solid black;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-start;
+}
+
+.nav-modal div {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nav-modal div:hover {
+  background: #ccc;
+}
+
+div p {
+  width: 100%;
+  text-align: left;
+}
+</style>
