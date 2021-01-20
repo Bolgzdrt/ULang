cf2d8b9e590eec8a2fb773c11514233afdb5edb0
Chris Jantzen <jantzeca@mail.uc.edu>
2021-01-19 22:50:18 -0500

    Express server cors

--- application/ulang/server/app.js
+++ application/ulang/server/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const { authRouter } = require('./routes/authRoutes')
 
 const app = express()
 
 app.use(express.json())
-app.use('/auth', authRouter)
+app.use(cors())
+app.use(authRouter)
 
 const PORT = 8081
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
