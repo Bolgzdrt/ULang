f1dd71f65f7b0a8208513b8a9d1ca7681d5ce75f
Bolgzdrt <robbob345@gmail.com>
2021-01-21 18:06:45 -0500

    added design for home page and home cards

---
+++ application/ulang/src/components/SetCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="actionCard">
+    <div class="name">
+     "set name"
+   </div>
+   <hr>
+   <div class="desc">
+     "set description"
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SetCard"
+}
+</script>
+
+<style scoped>
+.actionCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 5px;
+  width: 400px;
+  height: 200px;
+  filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
+  border: 2px solid #CCCCCC;
+  background: white;
+  text-align: center;
+  margin: 20px 5px 20px 5px;
+}
+
+hr {
+  width: 80%;
+  margin: 20px 0px 20px 0px;
+}
+
+div {
+  font-size: 26px;
+}
+
+.desc {
+  font-size: 18px;
+}
+
+.name {
+  padding-top: 20px;
+}
+</style>
--- application/ulang/src/views/Home.vue
+++ application/ulang/src/views/Home.vue
@@ -2,14 +2,27 @@
   <div class="home">
     <div class="quickStart">  
       <h1>Quick Start</h1>
-      
+      <div class="row">
+        <div class="cardColumn">
+          <SetCard />
+        </div>
+        <div class="cardColumn">
+          <QuickStartCard />
+        </div>
+      </div>
     </div>
     <div class="row">
       <div class="column">
         <h1>Recent Activity</h1>
+        <QuickStartCard />
+        <QuickStartCard />
+        <QuickStartCard />
       </div>
       <div class="column">
         <h1>Recent Sets</h1>
+        <SetCard />
+        <SetCard />
+        <SetCard />
       </div>
     </div>
   </div>
@@ -18,20 +31,51 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import QuickStartCard from '@/components/QuickStartCard.vue'
+import SetCard from '@/components/SetCard.vue'
 
 export default {
   name: 'Home',
   components: {
+    QuickStartCard,
+    SetCard
   }
 }
 </script>
 
 <style scoped>
+.quickStart {
+  text-align: left;
+}
+
 .row {
   display: flex;
+  text-align: left;
+}
+
+.cardColumn {
+  flex: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .column {
   flex: 50%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.home {
+  margin-left: 240px;
+  max-width: 1500px;
+  overflow: hidden;
+}
+
+h1 {
+  text-decoration: underline;
+  padding: 20px 0px 20px 50px;
+  align-self: start;
 }
 </style>
