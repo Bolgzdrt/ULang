5479eb3faf8da4b6efe6a108575dc0b139ce0807
Bolgzdrt <robbob345@gmail.com>
2021-01-27 20:11:14 -0500

    design and functionality done, ready to connect db

--- application/ulang/src/views/FlashCards.vue
+++ application/ulang/src/views/FlashCards.vue
@@ -2,23 +2,56 @@
   <div class="flashCards">
     <div class="set">
       <div class="name">Set Name</div>
-      <div class="progress">Progress: # / ##</div>
+      <div class="progress">Progress: {{ index + 1 }} / {{ total }}</div>
     </div>
-    <div class="card">
-      <div>omlette</div>
+    <div class="scene">
+      <div class="card" @click="toggleCard" v-bind:class="{isFlipped: flipped}">
+        <div class="cardFace">{{ words[index].english }}</div>
+        <div class="cardFace cardFaceBack">{{ words[index].translation }}</div>
+      </div>
     </div>
     <div class="nav">
-      <div class="cardChange leftButton"><img src="@/assets/pngs/Vector.png" alt="previous"></div>
+      <div class="cardChange leftButton" @click="prevCard"><img src="@/assets/pngs/Vector.png" alt="previous"></div>
       Click to flip
-      <div class="cardChange rightButton"><img src="@/assets/pngs/Vector.png" alt="next" style="transform: rotate(180deg);"></div>
+      <div class="cardChange rightButton" @click="nextCard"><img src="@/assets/pngs/Vector.png" alt="next" style="transform: rotate(180deg);"></div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'FlashCards'
+  name: 'FlashCards',
+  data() {
+    return {
+      words: [
+        { english: 'helmet', translation: 'casque' },
+        { english: 'egg', translation: 'oeuf' },
+        { english: 'diamond', translation: 'diamant' },
+        { english: 'dog', translation: 'chienne/chien' }
+      ],
+      index: 0,
+      total: 0,
+      flipped: false
+    }
+  },
+  methods: {
+    toggleCard: function() {
+      this.flipped = !this.flipped;
+    },
+    nextCard: function() {
+      if (!(this.index >= this.total - 1)) {
+        this.index += 1;
+      }
+    },
+    prevCard: function() {
+      if (!(this.index == 0)) {
+        this.index -= 1; 
+      }
+    },
+  },
+  created: function(){
+    this.total = this.words.length;
+  }
 }
 </script>
 
@@ -30,20 +63,6 @@ export default {
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
-}
-
-.card {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  background: #FFFFFF;
-  border-radius: 10px;
-  filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
-  border: 2px solid #CCCCCC;
-  font-size: 16px;
-  font-size: 4vw;
-  height: 50%;
-  width: 50%;
 }
 
 .name {
@@ -73,6 +92,10 @@ export default {
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  -webkit-user-select: none;  
+  -moz-user-select: none;    
+  -ms-user-select: none;      
+  user-select: none;
 }
 
 .cardChange:hover {
@@ -86,5 +109,49 @@ export default {
 
 .rightButton {
   margin-left: 20px;
+}
+
+.scene {
+  width: 50%;
+  height: 50%;
+  perspective: 1000px;
+}
+
+.card {
+  width: 100%;
+  height: 100%;
+  transition: transform 0.5s;
+  transform-style: preserve-3d;
+  cursor: pointer;
+  position: relative;
+}
+
+.card.isFlipped {
+  transform: rotateX(180deg);
+}
+
+.cardFace {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background: #FFFFFF;
+  border-radius: 10px;
+  filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
+  border: 2px solid #CCCCCC;
+  font-size: 16px;
+  font-size: 4vw;
+  -webkit-user-select: none;  
+  -moz-user-select: none;    
+  -ms-user-select: none;      
+  user-select: none;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.cardFaceBack {
+  transform: rotateX(180deg);
 }
 </style>
