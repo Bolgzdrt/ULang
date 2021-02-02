99a5bcd351e6cc5214a0b23a5032d093ba78d872
Bolgzdrt <robbob345@gmail.com>
2021-02-01 20:11:40 -0500

    added game and learn views, input card, and made component of back button

---
+++ application/ulang/src/components/InputCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="inputCard">
+     <div class="word">
+      <p class="word">Word</p>
+     </div>
+    <hr>
+    <div class="entry">
+      <input type="text" placeholder="Translation">
+      <button>Submit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputCard'
+}
+</script>
+
+<style scoped>
+.inputCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  width: 70%;
+  height: 70%;
+  filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
+  border: 2px solid #CCC;
+  background: white;
+  text-align: center;
+  margin: 20px 5px 20px 5px;
+  font-size: 20px;
+}
+
+hr {
+  width: 80%;
+  justify-self: center;
+}
+
+div {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+input {
+  width: 50%;
+  height: 15%;
+  margin-bottom: 2%;
+  text-align: center;
+  font-size: 100%;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 25px;
+}
+
+button {
+  height: 15%;
+  width: 50%;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 25px;
+  font-size: 100%;
+  transition: 0.1s;
+  box-shadow: 0 5px rgb(190, 190, 190);
+}
+
+button:hover {
+  filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
+  background: var(--purple);
+  cursor: pointer;
+}
+
+button:active {
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+
+.word {
+  font-size: 200%;
+}
+</style>
---
+++ application/ulang/src/views/Learn.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="learn">
+    <BackButton />
+    <SetProg v-bind:curr="index + 1" v-bind:total="total" v-bind:setName="setName" />
+    <InputCard />
+  </div>
+</template>
+
+<script>
+import BackButton from '@/components/BackButton.vue'
+import SetProg from '@/components/SetProg.vue'
+import InputCard from '@/components/InputCard.vue'
+
+export default {
+  name: 'Learn',
+  components: { BackButton, SetProg, InputCard },
+  data() {
+    return {
+      words: [
+        { english: 'helmet', translation: 'casque' },
+        { english: 'egg', translation: 'oeuf' },
+        { english: 'diamond', translation: 'diamant' },
+        { english: 'dog', translation: 'chienne/chien' }
+      ],
+      setName: 'Random Set',
+      index: 0,
+      total: 0
+    }
+  },
+  methods: {
+    
+  },
+  created: function(){
+    this.total = this.words.length;
+  }
+}
+</script>
+
+<style scoped>
+.learn {
+  width: 100%;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
