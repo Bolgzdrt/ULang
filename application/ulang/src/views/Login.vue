2785c8937194e13023fa471850985eab917c0312
Chris Jantzen <jantzeca@mail.uc.edu>
2021-02-10 17:09:54 -0500

    update with master

--- application/ulang/src/views/Login.vue
+++ application/ulang/src/views/Login.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="login">
+    <h1>Login</h1>
     <form @submit="onSubmit">
+      <div class="username-content">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" v-model="username">
+        <p class="error" v-if="usernameError">{{ usernameError }}</p>
+      </div>
+      <div class="password-content">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" v-model="password">
+        <p class="error" v-if="passwordError">{{ passwordError }}</p>
+      </div>
+      <button>Submit</button>
     </form>
+    <!-- Testing element -->
+    <button @click="logout" style="margin-top: 1rem;width:10%;">Reset</button>
   </div>
 </template>
 
@@ -50,7 +64,47 @@ export default {
 
 <style scoped>
 .login {
-  height: 100%;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.login h1 {
+  margin: 0.5rem;
+}
+
+.login form {
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+  height: 40%;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid #000;
+  border-radius: 10px;
+  padding: 1rem;
+}
+
+.login form .username-content,
+.login form .password-content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   width: 100%;
+}
+
+input {
+  width: 100%;
+  padding: 3px;
+}
+
+.login form button {
+  width: 30%;
+}
+
+.error {
+  color: red;
+  font-size: 0.75rem;
 }
 </style>
--- application/ulang/src/views/Signup.vue
+++ application/ulang/src/views/Signup.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="signup">
+    <h1>Signup</h1>
     <form @submit="onSubmit">
+      <div class="email-content">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" v-model="email" required />
+        <p class="error" v-if="emailError">{{ emailError }}</p>
+      </div>
+      <div class="username-content">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" pattern="^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$" v-model="username" required />
+        <p class="error" v-if="usernameError">{{ usernameError }}</p>
+      </div>
+      <div class="password-content">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+          v-model="password"
+          required
+        />
+        <p class="error" v-if="passwordError">{{ passwordError }}</p>
+      </div>
+      <button>Submit</button>
     </form>
+    <!-- Testing element -->
+    <button @click="logout" style="margin-top: 1rem;width:10%;">Reset</button>
   </div>
 </template>
 
@@ -58,7 +85,48 @@ export default {
 
 <style scoped>
 .signup {
-  height: 100%;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.signup h1 {
+  margin: 0.5rem;
+}
+
+.signup form {
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+  height: 40%;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid #000;
+  border-radius: 10px;
+  padding: 1rem;
+}
+
+.signup form .email-content,
+.signup form .username-content,
+.signup form .password-content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   width: 100%;
+}
+
+input {
+  width: 100%;
+  padding: 3px;
+}
+
+.signup form button {
+  width: 30%;
+}
+
+.error {
+  color: red;
+  font-size: 0.75rem;
 }
 </style>
