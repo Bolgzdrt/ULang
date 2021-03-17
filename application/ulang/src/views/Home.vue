d0954658675d7f819c0d5e6b10cf80713f34e3eb
Bolgzdrt <robbob345@gmail.com>
2021-03-17 16:41:43 -0400

    added default options for new users

--- application/ulang/src/views/Home.vue
+++ application/ulang/src/views/Home.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="home">
-    <div class="quickStart">  
+    <div class="quickStart" v-if="hasRecentList">  
       <h1>Recent Activity</h1>
       <div class="recentActRow">
         <div class="recentCard" v-for="act in recentActivity" :key="act._id">
           <QuickStartCard :setId="act.setId" :event="act.event" :eventSetting="act.setting" />
         </div>
-        <!-- <div class="cardColumn">
-          <QuickStartCard :setId="recentActivity[3].setId" :event="recentActivity[3].event" :eventSetting="recentActivity[3].setting" />
-          <QuickStartCard :setId="recentActivity[1].setId" :event="recentActivity[1].event" :eventSetting="recentActivity[1].setting" />
-        </div>
-        <div class="cardColumn">
-          <QuickStartCard :setId="recentActivity[2].setId" :event="recentActivity[2].event" :eventSetting="recentActivity[2].setting" />
-          <QuickStartCard :setId="recentActivity[0].setId" :event="recentActivity[0].event" :eventSetting="recentActivity[0].setting" />
-        </div> -->
       </div>
     </div>
     <div class="row">
@@ -22,11 +14,20 @@
         <div class="quickCard" v-for="set in quickAccessList" :key="set._id">
           <SetCard :set="set" />
         </div>
+        <div class="infoCard" v-if="hasQuickAccess">
+          Sets selected for quick access will appear here. From the user profile pages, click the lightning bolt icon to add the set to your quick access list.
+        </div>
       </div>
       <div class="column">
         <h1>Recently Created Sets</h1>
         <div class="quickCard" v-for="set in recentSetList" :key="set._id">
           <SetCard :set="set" />
+        </div>
+        <div class="infoCard" v-if="hasSetList">
+          <p>You do not have any created sets.</p>
+          <div class="buttonBox">
+            <button class="submitButton" @click="createSet">Create a Set</button>
+          </div>
         </div>
       </div>
     </div>
@@ -52,7 +53,10 @@ export default {
     return {
       quickAccessList: [],
       recentSetList: [],
-      recentActivity: []
+      recentActivity: [],
+      hasRecentList: false,
+      hasQuickAccess: false,
+      hasSetList: false
     }
   },
   methods: {
@@ -83,11 +87,23 @@ export default {
       } else {
         setTimeout(() => this.retrieveInfo(++tryNumber), 1000)
       }
+    },
+    createSet() {
+      this.$router.push({ name: 'CreateSet' })
     }
   },
   mounted() {
     this.retrieveInfo(1)
     this.recentActivity = getRecentList()
+    if(this.recentActivity.length > 0) {
+      this.hasRecentList = true
+    }
+    if(this.quickAccessList.length > 0) {
+      this.hasQuickAccess = true
+    }
+    if(this.recentSetList.length > 0) {
+      this.hasSetList = true
+    }
   }
 }
 </script>
@@ -141,5 +157,45 @@ h1 {
   text-decoration: underline;
   padding: 20px 0px 20px 50px;
   align-self: start;
+}
+
+.infoCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 5px;
+  width: 16em;
+  height: 10em;
+  filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
+  background: white;
+  text-align: center;
+  margin: 20px 5px 20px 5px;
+  padding: 15px;
+  font-size: 1.5em;
+}
+
+button {
+  height: 43px;
+  width: 145px;
+  border-radius: 5px;
+  font-size: 0.8em;
+  transition: 0.1s;
+  outline: none;
+  border: none;
+  filter: drop-shadow(3px 3px 4px rgba(0, 0, 0, 0.3));
+}
+button:hover {
+  filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.3));
+  cursor: pointer;
+}
+
+.submitButton {
+  background: var(--purple);
+  color: white;
+}
+
+.submitButton:hover {
+  background: #5b49d0;
 }
 </style>
